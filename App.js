44755075e4a717689afb229e601d6c5593f35ffb
carlaresende <dev.carlaresende@gmail.com>
2023-09-29 17:42:19 -0300

    first commmit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
 
+import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { useFonts } from 'expo-font';
+import { Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen';
+import Routes from './src/router';
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+    
+  });
+  
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <>
+   <View style={{flex:1}} onLayout={onLayoutRootView}>
+
+     <StatusBar style="light" backgroundColor="#000" translucent={false}/> 
+  <Routes/>   
+   </View>
+   
+  
+   </> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
